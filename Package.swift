ee5629df3ef85e131954a89d1bac5435de1ea238
aix <i@aix.uy>
2026-07-19 12:17:59 +0800

    chore: remove unused generated svg

--- Package.swift
+++ Package.swift
@@ -11,9 +11,6 @@ let package = Package(
         .executableTarget(
             name: "MousePortal",
             path: "MousePortal",
-            exclude: [
-                "overall_style_flat_2d_icon_style_minimalist_vector_019c9eb7-e897-7717-9394-4a0e4a674fb8.svg"
-            ],
             resources: [
                 .process("Resources")
             ]
